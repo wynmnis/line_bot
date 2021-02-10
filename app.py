f9491116bdbf06e8af6a75ab77498c18b2dca237
wynmnis <56291968+wynmnis@users.noreply.github.com>
2021-02-10 14:20:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
+    reply = TextSendMessage(text=f"!!{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
